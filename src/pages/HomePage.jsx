bb9e33835e61bcdcb676bd5d1fda53e7edc2937f
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2026-05-06 14:43:15 +0200

    ho fatto una chimata fetch all'interno dell'useEffect  con le dipendence vuote. in modo tale che la chiamata non viene ripetuta a ogni aggiornamento del componente. Ho stampato gli elementi della chiamta in pagina usando la variabile dell'useState dove ho salvato tuttigli elementi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+    const [makeupList, setMakeupList] = useState([])
 
+    useEffect(() => {
+        fetch(import.meta.env.VITE_API_URL)
+            .then(res => res.json())
+            .then(data => setMakeupList(data))
+            .catch(error => console.error(error));
+    }, [])
 
     return (
-        <>
-        </>
+        <div>
+            <h1>💄Catalogo trucchi</h1>
+            {makeupList.map((prodotto) => {
+                return (
+                    <div key={prodotto.id}>
+                        <h3>{prodotto.title}</h3>
+                        <p>{prodotto.category}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
