1843319b39245847c8f4ae7a7a22206cd6dd2bb4
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2026-05-06 19:20:41 +0200

    sistemata la navbar a livello grafico senza aver applicato alcuna logica aggiunto logo e barra di ricerca

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
+import logoMusa from "../assets/logo/logo.png";
+import { ShoppingCartIcon, MagnifyingGlassIcon } from "@phosphor-icons/react";
 
 
 export default function Navbar() {
     return (
-
         <nav className="navbar-container">
-            <Link to="/" className="navbar-link">🏠 Home</Link>
 
-            <span>ℹ️ Informazioni</span>
+            <div className="navbar-logo">
+                <img src={logoMusa} alt="Logo Musa Cosmetics" className="logo-img" />
+            </div>
+
+
+            <div className="navbar-links">
+                <Link to="/" className="navbar-link">HOME</Link>
+                <span className="navbar-link">PRODOTTI</span>
+                <span className="navbar-link">NOVITÀ</span>
+                <span className="navbar-link">BLOG</span>
+            </div>
+
+
+            <div className="navbar-search">
+                <span className="cart-icon"><ShoppingCartIcon size={32} color="#541926" /></span>
+
+                <div className="search-input-container">
+                    <input type="text" placeholder="Cerca i tuoi trucchi..." />
+                    <button className="search-btn"><MagnifyingGlassIcon size={32} color="#dce5ea" /></button>
+                </div>
+            </div>
+
         </nav>
     )
 }
