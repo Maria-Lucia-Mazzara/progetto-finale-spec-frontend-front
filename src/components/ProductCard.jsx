e2ace71ef5b7ea88c13b54de954f3340ea1a527d
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2026-05-07 13:04:26 +0200

    aggiunto anche il brand nelle card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,6 +11,7 @@ export default function ProductCard({ prodotto }) {
             </div>
 
             <h3 className="card-title">{prodotto.title}</h3>
+            <p className="card-brand">{prodotto.brand}</p>
             <p className="card-category">{prodotto.category}</p>
             <p className="card-price">{prodotto.price} €</p>
             <button className="card-btn">ACQUISTA</button>
